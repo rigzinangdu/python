50c31dddea798c1f175ff4868a72b02a5c4ea2c1
Rigzin Angdu <66792548+rigzinangdu@users.noreply.github.com>
2020-06-11 21:57:05 +0530

    Update and rename .practice to practice/input.py

---
+++ practice/input.py
@@ -0,0 +1,8 @@
+# We can make a input function in python
+
+name = input("Enter the Name : ")
+age = int(input("Enter the age : ")) # it's should be comes in Integer format 
+place = input("Enter the place : ")
+contact = input("Enter the contact number : ")
+
+print(f"Hello {name} Your age is {age} You are from {place} and Your Contact number is {contact}") #string formatting Python V3.6
