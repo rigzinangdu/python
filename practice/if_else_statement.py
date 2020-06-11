110c81cc0ea70ba2c8f428693bccb5a7dfd03281
Rigzin Angdu <66792548+rigzinangdu@users.noreply.github.com>
2020-06-11 22:05:03 +0530

    Update and rename if_statement.py to if_else_statement.py

---
+++ practice/if_else_statement.py
@@ -0,0 +1,12 @@
+#What are the uses of the if Statement in Python :
+
+name = "python"
+pass1 = "abc123456"
+username = input("Enter the username : ")
+password = input("Enter your password : ")
+if username == name and password == pass1:    #we can use if_staement 
+  print("Line A")
+  print("Login Successfully")
+else:                                           #we can use else_statement | Remember else comes after if statement.
+  print("invalid username and password")
+
