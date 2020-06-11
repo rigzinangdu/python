5a0315aa3c39bae66db01e1beccbdbffa88f90d8
Rigzin Angdu <66792548+rigzinangdu@users.noreply.github.com>
2020-06-11 22:00:50 +0530

    Create if_statement.py

---
+++ practice/if_statement.py
@@ -0,0 +1,8 @@
+#What are the uses of the if Statement in Python :
+
+name = "python"
+pass1 = "abc123456"
+username = input("Enter the username : ")
+password = input("Enter your password : ")
+if username == name and password == pass1:
+
