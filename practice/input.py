a4d9197e23f0ca84b3d2e296f8a3189843815aa8
Rigzin Angdu <66792548+rigzinangdu@users.noreply.github.com>
2020-06-13 13:33:11 +0530

    Update input.py

--- practice/input.py
+++ practice/input.py
@@ -3,6 +3,8 @@
 name = input("Enter the Name : ")
 age = int(input("Enter the age : ")) # it's should be comes in Integer format 
 place = input("Enter the place : ")
-contact = input("Enter the contact number : ")
+contact = int(input("Enter the contact number : "))
 
-print(f"Hello {name} Your age is {age} You are from {place} and Your Contact number is {contact}") #string formatting Python V3.6
+#string formatting Python V3.6
+print(f"Hello {name} Your age is {age} You are from {place} and Your Contact number is {contact}")
+
