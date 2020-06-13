0468348a92cd5e877cf6ebdc03a566b7c0b198ce
Rigzin Angdu <66792548+rigzinangdu@users.noreply.github.com>
2020-06-13 13:40:03 +0530

    Update elif_statement.py

--- practice/elif_statement.py
+++ practice/elif_statement.py
@@ -1,9 +1,5 @@
 age = int(input("Enter the age : ")
-elif 0 < age < 6:
+if 0 < age < 6:
           print("Your Ticket is Free")
-elif 6 < age < 13:
-          print("Your Ticket Price Rs.10")
-elif 13 < age < 61:
-          print ("Your Ticket Price Rs.20")
 else:
           print("You are not eligible")
